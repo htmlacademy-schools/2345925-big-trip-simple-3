b597d261137f32b2b2bbc5952cbdacd7a5c02fd1
Dimboba <lazarkovdmitry@yandex.ru>
2023-04-18 12:55:57 +0300

    Начинаем программировать

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+const path = require('path');
+const CopyPlugin = require("copy-webpack-plugin");
+
+module.exports = {
+  entry: './src/main.js',
+  output: {
+    filename: 'bundle.js',
+    path: path.resolve(__dirname, 'build'),
+    clean: true,
+  },
+  devtool: 'source-map',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: ['babel-loader']
+      }
+    ]
+  }
+}
