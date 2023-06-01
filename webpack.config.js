502456eb597705e9b01b674819bdfee7c87b580b
Dimboba <lazarkovdmitry@yandex.ru>
2023-06-01 14:57:17 +0300

    module1-task3

--- webpack.config.js
+++ webpack.config.js
@@ -9,13 +9,22 @@ module.exports = {
     clean: true,
   },
   devtool: 'source-map',
+  plugins: [
+    new CopyPlugin({
+      patterns: [{ from: 'public' }],
+    }),
+  ],
   module: {
     rules: [
       {
         test: /\.js$/,
         exclude: /(node_modules)/,
         use: ['babel-loader']
+      },
+      {
+        test: /\.css$/i,
+        use: ['style-loader', 'css-loader']
       }
     ]
   }
-}
+};
