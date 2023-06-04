6d8db016ce6ddb59e7845c74e2440f5df818afe0
Dimboba <lazarkovdmitry@yandex.ru>
2023-06-04 21:48:27 +0300

    module2-task1

---
+++ src/mock/destination.js
@@ -0,0 +1,31 @@
+import {generateRandomInt, generateRandomImageUrl} from '../utils/random';
+
+const names = ['Chamonix', 'Berlin', 'Moscow', 'Tver', 'NY', 'Petrozavodsk'];
+
+
+const generatePicture = () => ({
+  src: generateRandomImageUrl(),
+  description: 'something'
+});
+
+export const generateDestination = (id) => ({
+  id,
+  description: 'something something',
+  name: names[generateRandomInt(0, names.length - 1)],
+  pictures:
+    Array.from({length: generateRandomInt(2, 10)}, generatePicture)
+});
+
+
+export const randomDestinations = (() => {
+  const destinations = [];
+
+  for (let i = 0; i <= 10; i++) {
+    destinations.push(generateDestination(i));
+  }
+
+  return {
+    getDestination: (id) => destinations[id],
+    getAllDestinations: () => destinations,
+  };
+})();
