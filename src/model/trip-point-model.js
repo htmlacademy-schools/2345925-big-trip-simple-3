587cef88e3890434c215c661e68d41872272bdba
Dimboba <lazarkovdmitry@yandex.ru>
2023-06-07 12:54:55 +0300

    module4-task2 (refactor with OOP)

--- src/model/trip-point-model.js
+++ src/model/trip-point-model.js
@@ -1,6 +1,6 @@
 import { generateRandomPoint } from '../mock/point';
 
-const POINT_COUNT = 5;
+const POINT_COUNT = 4;
 
 export default class TripPointModel {
   tripPoints = Array.from({length: POINT_COUNT}, generateRandomPoint);
