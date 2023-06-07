33ddbbbf9968804654791034427ff9b9d1c89647
Dimboba <lazarkovdmitry@yandex.ru>
2023-06-07 12:25:16 +0300

    module3-task2 (empty list message added)

--- src/model/trip-point-model.js
+++ src/model/trip-point-model.js
@@ -1,6 +1,6 @@
 import { generateRandomPoint } from '../mock/point';
 
-const POINT_COUNT = 0;
+const POINT_COUNT = 1;
 
 export default class TripPointModel {
   tripPoints = Array.from({length: POINT_COUNT}, generateRandomPoint);
